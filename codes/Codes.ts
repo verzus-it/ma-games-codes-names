136604f20fee7141c134796c1c274b672cb2f5c5
Alexander Bryuhanov <br.sani4@gmail.com>
2019-09-05 12:54:34 +0300

    removed ts

--- codes/Codes.ts
+++ codes/Codes.ts
@@ -3,13 +3,13 @@ import {ArrayUtils} from 'ts-data-structures-utils';
 import {Code} from './Code';
 
 export class Codes {
-    static getAll():Array<any> {
+    static getAll() {
         return Object.values(Code)
             .filter(code => typeof code === 'number');
     }
 
     static getRandom(availableCodes:any = null):any {
-        const codes:Array<any> = availableCodes && availableCodes.length > 0
+        const codes = availableCodes && availableCodes.length > 0
             ? availableCodes
             : this.getAll();
 
