95cdba39633536e05fbd3b4692529f774ac87db0
Alexander Bryuhanov <br.sani4@gmail.com>
2019-09-05 12:37:31 +0300

    Hot fix commit

--- codes/Codes.ts
+++ codes/Codes.ts
@@ -3,16 +3,16 @@ import {ArrayUtils} from 'ts-data-structures-utils';
 import {Code} from './Code';
 
 export class Codes {
-    static getAll():Array<Code> {
+    static getAll():Array<any> {
         return Object.values(Code)
             .filter(code => typeof code === 'number');
     }
 
-    static getRandom(availableCodes:Code[]|null = null):Code {
-        const codes:Array<Code> = availableCodes && availableCodes.length > 0
+    static getRandom(availableCodes:any = null):any {
+        const codes:Array<any> = availableCodes && availableCodes.length > 0
             ? availableCodes
             : this.getAll();
 
-        return ArrayUtils.getRandom<Code>(codes);
+        return ArrayUtils.getRandom(codes);
     }
 }
